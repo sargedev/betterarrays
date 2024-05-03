6f6c33b25307a928908b49dc6224b38764b4ad77
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2024-05-03 18:07:24 +0200

    Written test for find method

--- test.ts
+++ test.ts
@@ -1,1 +1,19 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+
+let result;
+
+function testFind() {
+    // Test find
+    result = arrays.find(["a", "b", "c"], "c");
+    new tests.AssertEqual(result, 2);
+
+    // Test find first occurence (of multiple)
+    result = arrays.find(["a", "b", "b"], "b");
+    new tests.AssertEqual(result, 1);
+
+    // Test not found
+    result = arrays.find(["a", "b", "c"], "d");
+    new tests.AssertEqual(result, -1);
+}
+
+testFind();
