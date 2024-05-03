92b78e23a7babe07bf54990156ef63b6cbd48d90
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2024-05-03 17:31:10 +0200

    Rewrote find method using findAll; added comments

--- main.ts
+++ main.ts
@@ -1,13 +1,29 @@
 
 namespace arrays {
-    export function find(array: any[], item: any) {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] === item) return i;
-        }
-        return -1;
+    /**
+     * Find first occurence of item in array
+     * @param array Array to search
+     * @param item Item to find
+     * @returns Index of found item
+     */
+    export function find(array: any[], item: any): number {
+        let result = findAll(array, item, 1);
+        return result.length ? result[0] : -1;
     }
 
-    export function findAll(array: any[], item: any) {
-        
+    /**
+     * Find all occurences of item in array
+     * @param array Array to search
+     * @param item Item to find
+     * @param max Max number of ocurrences to match
+     * @returns Array of indicies 
+     */
+    export function findAll(array: any[], item: any, max: number): number[] {
+        let result = [];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === item) result.push(i);
+            if (result.length >= max) break;
+        }
+        return result;
     }
 }
