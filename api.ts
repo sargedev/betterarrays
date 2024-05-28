76f771eaf82370d5960414e6bbbc5cba9bfce6fd
Sarge <63020240+sargedev@users.noreply.github.com>
2024-05-28 20:29:01 +0200

    Fixed block syntax

--- api.ts
+++ api.ts
@@ -1206,7 +1206,7 @@ namespace arrays {
      * @param max Maximum depth (0 is unlimited)
      */
     //% blockId=arrays_flatten
-    //% block="flatten $array with depth $max"
+    //% block="flatten $array || with depth $max"
     //% group="Modify"
     //% array.shadow=variables_get
     //% array.defl=list
@@ -1224,7 +1224,7 @@ namespace arrays {
      * @returns Flattened array
      */
     //% blockId=arrays_toFlattened
-    //% block="flattened $array with depth $max"
+    //% block="flattened $array || with depth $max"
     //% group="Operations"
     //% array.shadow=variables_get
     //% array.defl=list
